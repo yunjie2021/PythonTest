e6d25fbb4da91d095492f0b8974a32f33340f455
lyj <liu.yunjie@qq.com>
2021-06-01 11:03:37 +0800

    210601字典的用法

--- test.py
+++ test.py
@@ -1,20 +1,16 @@
 
-from enum import Enum
-class Color(Enum):
-    RED = 1
-    GREEN = 2
-    BLUE = 3
-
-print(Color.RED)
-
-a = 20
-a = a + Color.RED.value
-print(a)
-
-b = Color.GREEN
-print(b.name)
-print(type(b.name))
-print(b.value)
-
-c = Color()   #error
-c.name = "RED"  #error
+根蒂 = {'蜷缩':0, '稍蜷':1, '硬挺':2}
+脐部 = {'凹陷':0, '稍凹':1, '平坦':2}
+print(根蒂)
+print(根蒂.items())
+print(根蒂.keys())
+print(根蒂.values())
+print(根蒂['蜷缩'])
+print('======')
+print(根蒂.get('硬挺'))
+print(根蒂.get('硬', None))
+print(type(None))
+根蒂['直立'] = 9
+print(根蒂)
+print(根蒂.pop('直立'))
+print(根蒂)
