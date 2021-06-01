ceba41f836d248d10d5b134e11973931378572c5
lyj <liu.yunjie@qq.com>
2021-06-01 18:16:18 +0800

    eval的用法把字符转变成变量

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
-
-根蒂 = {'蜷缩':0, '稍蜷':1, '硬挺':2}
 脐部 = {'凹陷':0, '稍凹':1, '平坦':2}
+根蒂 = {'蜷缩':0, '稍蜷':1, '硬挺':2}
 print(根蒂)
 print(根蒂.items())
 print(根蒂.keys())
@@ -14,3 +13,10 @@ print(type(None))
 print(根蒂)
 print(根蒂.pop('直立'))
 print(根蒂)
+print("-----")
+a = ['脐部','根蒂']
+print(a)
+print(type(a))
+print(a[0])
+print(eval(a[1])['蜷缩'])
+print(1^1)    #注意，要做异或必须是整数。
