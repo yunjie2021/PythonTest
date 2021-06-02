4b032a6bba554ac8e8a499ba7a324e9703377a42
lyj <liu.yunjie@qq.com>
2021-06-02 10:24:47 +0800

    plot用法1

--- test.py
+++ test.py
@@ -1,22 +1,13 @@
-脐部 = {'凹陷':0, '稍凹':1, '平坦':2}
-根蒂 = {'蜷缩':0, '稍蜷':1, '硬挺':2}
-print(根蒂)
-print(根蒂.items())
-print(根蒂.keys())
-print(根蒂.values())
-print(根蒂['蜷缩'])
-print('======')
-print(根蒂.get('硬挺'))
-print(根蒂.get('硬', None))
-print(type(None))
-根蒂['直立'] = 9
-print(根蒂)
-print(根蒂.pop('直立'))
-print(根蒂)
-print("-----")
-a = ['脐部','根蒂']
-print(a)
-print(type(a))
-print(a[0])
-print(eval(a[1])['蜷缩'])
-print(1^1)    #注意，要做异或必须是整数。
+import math
+import matplotlib.pyplot as plt
+
+nbSamples = 256
+xRange = (-math.pi, math.pi)
+x, y = [], []
+
+for n in range(nbSamples):
+    ratio = (n+0.5) / nbSamples
+    x.append(xRange[0] + (xRange[1] - xRange[0]) * ratio)
+    y.append(math.sin(x[-1]))
+plt.plot(x,y)
+plt.show()
